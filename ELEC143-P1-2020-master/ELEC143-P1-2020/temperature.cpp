9358f381a12f782a896bd9fbc7392cda9f33991e
Wattisforpower <65559808+Wattisforpower@users.noreply.github.com>
2020-11-24 15:58:37 +0000

    Update temperature.cpp
    I have fixed the problem with temperature.cpp and added hysterersis with it.

--- ELEC143-P1-2020-master/ELEC143-P1-2020/temperature.cpp
+++ ELEC143-P1-2020-master/ELEC143-P1-2020/temperature.cpp
@@ -1,17 +1,22 @@
 //#include "mbed.h"
 #include "../lib/uopmsb/uop_msb_2_0_0.h"
 #include "BMP280_SPI.h"
+#include <cstring>
 
 using namespace uop_msb_200;
 
+// Using the External Buzzer
 extern Buzzer buzz;
 
+
+// Initialising the DigitalOut only for this file 
 static DigitalOut traf1RedLED(TRAF_RED1_PIN,1);
 static DigitalOut traf1YelLED(TRAF_YEL1_PIN);
 static DigitalOut traf1GrnLED(TRAF_GRN1_PIN);
 
+// This file only function which works out the sum of the array
 static float Sum_of_Temperature(float *array){
-    float Sum_of_Temperature_Array;
+    float Sum_of_Temperature_Array = 0;
     int Sum_Index;
 
     for (Sum_Index = 0; Sum_Index < 10; Sum_Index++){
@@ -22,19 +27,26 @@ static float Sum_of_Temperature(float *array){
     return Sum_of_Temperature_Array;
 }
 
+/*
+ Main function in this file which changes which lights are turned on at different temperatures and
+ will turn on the buzzer when it goes below freezing, Hysteresis has also been used as shown in the
+ comment section int the main function below
+*/
 void temperature_function(float temperature_sensor){
     
     /*
-        Ranges:      | With Hysterysis:
-        x < 0        | x < 2
-        0 < x < 20   | -2 < x < 22
-        20 < x < 40  | 18 < x < 42
-        40 < x       | 38< x
+        Ranges:      | With Hysteresis:
+             x < 0   |      x < -1
+         0 < x < 20  |  1 < x < 19
+        20 < x < 40  | 21 < x < 39
+        40 < x       | 41 < x
     */
     float temperature_values[10]; // Takes in the rolling average
     float Av_Temperature; // Average Temperature
     int Temp_Index;
     float Sum_Temperature;
+
+    memset(temperature_values, 0, size_t(10)); // Sets all elements of the array to 0
 
     for (Temp_Index = 0; Temp_Index < 10; Temp_Index++){
         temperature_values[Temp_Index] = temperature_sensor;
@@ -44,7 +56,7 @@ void temperature_function(float temperature_sensor){
 
     Av_Temperature = Sum_Temperature / 10;
 
-    if (Av_Temperature < 2.0){
+    if (Av_Temperature < -1 ){
 
         buzz.playTone("A", Buzzer::MIDDLE_OCTAVE);
         traf1RedLED = 1;
@@ -55,17 +67,17 @@ void temperature_function(float temperature_sensor){
         traf1RedLED = 0;
         wait_us(1000000);
     }
-    else if (-2.0 < Av_Temperature < 22.0){
+    else if ((1 < Av_Temperature) && (Av_Temperature < 19)){
         traf1RedLED = 1;
         traf1YelLED = 0;
         traf1GrnLED = 0;
     }
-    else if (18.0 < Av_Temperature < 42.0){
+    else if ((21 < Av_Temperature) && (Av_Temperature < 39)){
         traf1RedLED = 0;
         traf1YelLED = 1;
         traf1GrnLED = 0;
     }
-    else if (38.0 < Av_Temperature){
+    else if (41 < Av_Temperature){
         traf1RedLED = 0;
         traf1YelLED = 0;
         traf1GrnLED = 1;
