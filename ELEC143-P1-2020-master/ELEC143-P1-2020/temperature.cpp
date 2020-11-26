cc01dcd8d75db8a226c7b2296dbdfd618ef228fa
Wattisforpower <65559808+Wattisforpower@users.noreply.github.com>
2020-11-26 07:08:54 +0000

    Update temperature.cpp
    Added a detection for when category changes and it will display text accordingly

--- ELEC143-P1-2020-master/ELEC143-P1-2020/temperature.cpp
+++ ELEC143-P1-2020-master/ELEC143-P1-2020/temperature.cpp
@@ -14,6 +14,8 @@ static DigitalOut traf1RedLED(TRAF_RED1_PIN,1);
 static DigitalOut traf1YelLED(TRAF_YEL1_PIN);
 static DigitalOut traf1GrnLED(TRAF_GRN1_PIN);
 
+
+
 // This file only function which works out the sum of the array
 static float Sum_of_Temperature(float *array){
     float Sum_of_Temperature_Array = 0;
@@ -26,6 +28,19 @@ static float Sum_of_Temperature(float *array){
 
     return Sum_of_Temperature_Array;
 }
+
+// This File only Function which will detect when the category changes and will report it;
+static void Is_Category_Change(int Cat){
+    static int old_value;
+    if (Cat > 0){
+        if (Cat != old_value){
+            printf("A Category Change is occuring \n");
+        }
+    }
+    old_value = Cat;
+
+}
+
 
 /*
  Main function in this file which changes which lights are turned on at different temperatures and
@@ -45,8 +60,9 @@ void temperature_function(float temperature_sensor){
     float Av_Temperature; // Average Temperature
     int Temp_Index;
     float Sum_Temperature;
+    short Category_Value;
 
-    memset(temperature_values, 0, size_t(10)); // Sets all elements of the array to 0
+    memset(temperature_values, 0, size_t(10)); // Sets all elements of the temperature_values array to 0
 
     for (Temp_Index = 0; Temp_Index < 10; Temp_Index++){
         temperature_values[Temp_Index] = temperature_sensor;
@@ -57,7 +73,7 @@ void temperature_function(float temperature_sensor){
     Av_Temperature = Sum_Temperature / 10;
 
     if (Av_Temperature < -1 ){
-
+        Category_Value = 1;
         buzz.playTone("A", Buzzer::MIDDLE_OCTAVE);
         traf1RedLED = 1;
         traf1YelLED = 0;
@@ -68,19 +84,25 @@ void temperature_function(float temperature_sensor){
         wait_us(1000000);
     }
     else if ((1 < Av_Temperature) && (Av_Temperature < 19)){
+        Category_Value = 2;
         traf1RedLED = 1;
         traf1YelLED = 0;
         traf1GrnLED = 0;
     }
     else if ((21 < Av_Temperature) && (Av_Temperature < 39)){
+        Category_Value = 3;
         traf1RedLED = 0;
         traf1YelLED = 1;
         traf1GrnLED = 0;
     }
     else if (41 < Av_Temperature){
+        Category_Value = 4;
         traf1RedLED = 0;
         traf1YelLED = 0;
         traf1GrnLED = 1;
     }
+    
+    Is_Category_Change(Category_Value);
 
+    printf("Category: %d, Average Temp: %.1f | ", Category_Value, Av_Temperature);
 }
